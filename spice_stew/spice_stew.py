df4eb9d2dd8fe548772a226fe02491ed93337183
Gabriel Pelouze <gabriel@pelouze.net>
2022-12-01 17:20:04 +0100

    update output filenames to match spice_jitter_correction

--- spice_stew/spice_stew.py
+++ spice_stew/spice_stew.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
+import copy
 import glob
 import itertools
 import os
+import re
 import warnings
 
 from astropy import wcs
@@ -23,6 +25,102 @@ except ImportError:
 
 
 __all__ = ['SpiceSpicePointing', 'correct_spice_pointing']
+
+
+class SpiceFilename(dict):
+    re_spice_L123_filename = re.compile(
+        r'''
+        solo
+        _(?P<level>L[123])
+        _spice
+            (?P<concat>-concat)?
+            -(?P<slit>[wn])
+            -(?P<type>(ras|sit|exp))
+            (?P<db>-db)?
+            (?P<int>-int)?
+        _(?P<time>\d{8}T\d{6})
+        _(?P<version>V\d{2})
+        _(?P<SPIOBSID>\d+)-(?P<RASTERNO>\d+)
+        (?P<ext>\..*)
+        ''',
+        re.VERBOSE
+        )
+
+    def __init__(self, filename):
+        """ Represent SPICE filename
+
+        Parameters
+        ----------
+        filename : str
+            eg: /a/b/solo_L2_spice-n-ras_20210914T025031_V03_67109159-000.fits
+        """
+        super().__init__(self._parse_filename(filename))
+        self._original_filename = filename
+
+        self._check_filename_reconstruction()
+
+    def to_str(self, **kw):
+        """ Generate filename string
+
+        Parameters
+        ----------
+        **kw :
+            If specified, these keywords are replaced in the generated filename
+            If not specified, the default values are used. Supported
+            keywords are:
+            - level ('L1', 'L2', ...)
+            - concat ('-concat' or '')
+            - slit ('n' or 'w')
+            - type ('ras', 'sit', 'exp')
+            - db ('-db' or '')
+            - int ('-int' or '')
+            - time (YYYYMMDDTHHMMSS)
+            - version ('V03', ...)
+            - SPIOBSID ('67109159', ...)
+            - RASTERNO ('000', ...)
+            - ext ('.fits', '.fits.gz', ...)
+
+        Returns
+        -------
+        filename : str
+            New filename with interpolated keywords.
+        """
+        if kw:
+            props = self.copy()
+            props.update(**kw)
+        else:
+            props = self
+        basename = ('solo_{level}_spice{concat}-{slit}-{type}{db}{int}'
+                    '_{time}_{version}_{SPIOBSID}-{RASTERNO}{ext}')
+        filename = os.path.join(
+            props['path'],
+            basename.format(**props),
+            )
+        return filename
+
+    def _parse_filename(self, filename):
+        path, basename = os.path.split(filename)
+        m = self.re_spice_L123_filename.match(basename)
+        if m is None:
+            raise ValueError(f'could not parse SPICE filename: {basename}')
+        d = m.groupdict()
+        for k, v in d.items():
+            if v is None:
+                d[k] = ''
+        d['path'] = path
+        return d
+
+    def _check_filename_reconstruction(self):
+        filename = self.to_str()
+        msg = (f"Filename reconstruction error: "
+               f"{self._original_filename} -> {filename}")
+        assert filename == self._original_filename, msg
+
+    def __repr__(self):
+        return f'<{type(self).__name__} {self}>'
+
+    def __str__(self):
+        return self.to_str()
 
 
 class SpiceSpicePointing():
@@ -306,18 +404,21 @@ def correct_spice_pointing(spice_spice_pointing, filename, output_dir,
     '''
     os.makedirs(output_dir, exist_ok=True)
     # filename operations
-    basename = os.path.splitext(os.path.basename(filename))[0]
+    filename = SpiceFilename(filename)
+    filename_output = copy.copy(filename)
+    filename_output['path'] = output_dir
     if sum_wvl:
-        output_fits = f'{output_dir}/{basename}_remapped_img.fits'
+        filename_output['level'] = 'L2r_quicklook'
     else:
-        output_fits = f'{output_dir}/{basename}_remapped.fits'
+        filename_output['level'] = 'L2r'
+    output_fits = filename_output.to_str()
 
     if os.path.isfile(output_fits) and not overwrite:
         print(f'Aligned file exists: {output_fits}, exiting')
         return output_fits
 
     # open FITS
-    hdulist = fits.open(filename)
+    hdulist = fits.open(filename.to_str())
     timestamps = get_spice_timestamps(hdulist)
 
     # determine pointing
@@ -344,13 +445,16 @@ def correct_spice_pointing(spice_spice_pointing, filename, output_dir,
         spice_name = os.path.basename(hdulist.filename())
         p.plot_pointing(
             timestamps, Tx, Ty, roll, spice_name,
-            f'{output_dir}/{basename}_plot_TxTy.pdf')
+            filename_output.to_str(ext='_plot_TxTy.pdf'),
+            )
         p.plot_hdulist(
             hdulist, spice_name,
-            f'{output_dir}/{basename}_original.pdf')
+            filename_output.to_str(ext='_plot_not_aligned.pdf'),
+            )
         p.plot_hdulist(
             new_hdulist, spice_name,
-            f'{output_dir}/{basename}_remapped.pdf')
+            filename_output.to_str(ext='_plot_aligned.pdf'),
+            )
 
     return output_fits
 
